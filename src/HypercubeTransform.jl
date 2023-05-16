3e6a6b912172e1eca1ee99f95103f4831bd09aec
Paul Tiede <ptiede91@gmail.com>
2023-05-16 13:27:24 -0400

    Add some precompile statements

--- src/HypercubeTransform.jl
+++ src/HypercubeTransform.jl
@@ -16,6 +16,7 @@ import TransformVariables: as, transform, inverse, inverse!, inverse_eltype, dim
 import TransformVariables
 const TV = TransformVariables
 using Random: AbstractRNG
+using PrecompileTools
 export transform, inverse, dimension, ascube, asflat, transform_and_logjac, transform_logdensity
 
 include("utility.jl")
@@ -23,6 +24,28 @@ include("transform.jl")
 include("inverse.jl")
 include("composite.jl")
 include("asflat.jl")
+
+
+@setup_workload begin
+    a = Dists.Normal()
+    b = Dists.Uniform()
+    c = Dists.product_distribution([Dists.Normal(), Dists.Normal()])
+    d = Dists.Gamma()
+    pos = [0.5, 0.5, 0.5, 0.5, 0.1]
+
+    tc = ascube((a, b, c, d))
+    nc = ascube((;a,b, c, d))
+
+    tf = asflat((a,b,c,d))
+    nf = asflat((;a,b,c,d))
+
+    pt = transform(tc, pos)
+    pn = transform(nc, pos)
+
+    ptf = transform(tf, pos)
+    pnf = transform(nf, pos)
+end
+
 
 #function __init__()
  #   @require Soss="8ce77f84-9b61-11e8-39ff-d17a774bf41c" include("soss.jl")
