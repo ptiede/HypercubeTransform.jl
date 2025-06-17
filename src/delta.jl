b8a6eef68fd15e95938ce2256c2563c1af3e57b6
Paul Tiede <ptiede91@gmail.com>
2025-06-17 13:18:46 -0400

    Format

--- src/delta.jl
+++ src/delta.jl
@@ -6,21 +6,21 @@ end
 
 Base.length(d::DeltaDist{<:Number}) = 1
 Base.length(d::DeltaDist{<:AbstractArray}) = length(d.x0)
-Base.eltype(::DeltaDist{T}) where {T<:Number} = T
+Base.eltype(::DeltaDist{T}) where {T <: Number} = T
 Base.eltype(::DeltaDist{<:AbstractArray{T}}) where {T} = T
 Dists.insupport(d::DeltaDist, x) = true # always in support because we don't want to error out
 
-function Dists.logpdf(::DeltaDist{T}, x::Real) where {T<:Real}
+function Dists.logpdf(::DeltaDist{T}, x::Real) where {T <: Real}
     return zero(T) # Ignore the case when we equal since that is measure zero anyways
 end
 
-function Dists.__logpdf(d::DeltaDist{<:AbstractArray{T}}, x::AbstractArray{T}) where {T<:Real}
+function Dists.__logpdf(d::DeltaDist{<:AbstractArray{T}}, x::AbstractArray{T}) where {T <: Real}
     @assert length(x) == length(d.x0) "Input vector must be the same length as the delta distribution"
     return zero(eltype(x))
 end
 
 function Dists.product_distribution(dists::AbstractVector{<:DeltaDist})
-    x0 = mapreduce(x->x.x0, vcat, dists)
+    x0 = mapreduce(x -> x.x0, vcat, dists)
     return DeltaDist(x0)
 end
 
@@ -28,7 +28,7 @@ function Dists.rand(rng::AbstractRNG, d::DeltaDist{<:Real})
     return d.x0 # always return the same value
 end
 
-function Dists._rand!(::AbstractRNG, d::DeltaDist{<:AbstractArray{T}}, x::AbstractArray{T}) where {T<:Real}
+function Dists._rand!(::AbstractRNG, d::DeltaDist{<:AbstractArray{T}}, x::AbstractArray{T}) where {T <: Real}
     @assert length(x) == length(d.x0) "Input vector must be the same length as the delta distribution"
     x .= d.x0 # always return the same value
     return x
@@ -36,9 +36,3 @@ end
 
 asflat(d::DeltaDist) = TV.Constant(d.x0)
 ascube(d::DeltaDist) = TV.Constant(d.x0)
-
-
-
-
-
-
